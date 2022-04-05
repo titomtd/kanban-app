52ffb50d4e49671bfe24fcc7516c9ed554431fe8
titomtd <thomasduchemin.pro@gmail.com>
2022-04-05 10:18:32 +0200

    Make board detail component

---
+++ src/app/board-detail/board-detail.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import {Board} from "../model/board.model";
+import {BoardService} from "../service/board/board.service";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Section } from "../model/section.model";
+import {SectionService} from "../service/section/section.service";
+
+@Component({
+  selector: 'app-board-detail',
+  templateUrl: './board-detail.component.html',
+  styleUrls: ['./board-detail.component.scss']
+})
+export class BoardDetailComponent implements OnInit {
+  faTimes = faTimes;
+
+  board!: Board;
+  sections: Section[] = [];
+
+  formNewSectionShow: boolean = false;
+  formNewSection!: FormGroup;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private boardService: BoardService,
+    private formBuilder: FormBuilder,
+    private sectionService: SectionService
+  ) {
+    this.formNewSection = this.formBuilder.group({
+      label: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(16)]),
+      position: new FormControl('', [Validators.required])
+    })
+  }
+
+  ngOnInit(): void {
+    this.loadBoard();
+  }
+
+  public loadBoard(): void {
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.boardService.getBoard(params.get('boardId')).subscribe(
+        item => {
+          this.board = item;
+          this.sections = this.board.sections.sort( (a, b) => a.position - b.position);
+        }
+      );
+    });
+  }
+
+  createNewSection() {
+    if (this.formNewSection.valid) {
+      this.boardService.addNewSection(this.board.id, this.formNewSection.value).subscribe(
+        item => {
+          this.board = item;
+          this.sections = this.board.sections.sort( (a, b) => a.position - b.position);
+          this.toggleFormNewSection();
+        }
+      );
+    }
+  }
+
+  toggleFormNewSection(): void {
+    this.formNewSectionShow = !this.formNewSectionShow;
+  }
+
+  public deleteSection($event: any): void {
+    this.sectionService.deleteSection($event).subscribe(_ => this.loadBoard());
+  }
+}
